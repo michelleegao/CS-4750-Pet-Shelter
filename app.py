c863a6ed9c5855a46faf56cc71760acc5cf99539
michelleegao <163981352+michelleegao@users.noreply.github.com>
2026-04-10 12:42:02 -0400

    added navbar and search bar in views page

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# links login HTML page
 @app.route("/")
 def home():
     return render_template('login.html')
+
+# links pets view HTML page
+@app.route("/pets")
+def pets():
+    return render_template('pets_view.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
