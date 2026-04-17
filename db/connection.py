263f178d7d2dd937d82ae951b021dbc68ac09cfa
michelleegao <163981352+michelleegao@users.noreply.github.com>
2026-04-17 12:22:59 -0400

    added gitignore file

--- db/connection.py
+++ db/connection.py
@@ -1,14 +1,23 @@
 from google.cloud.sql.connector import Connector
 import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 connector = Connector()
 
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+PROJECT_ID = os.getenv("PROJECT_ID")
+
 def getconn():
     conn = connector.connect(
-        "cs4750-data:us-east4:pets-2",
+        PROJECT_ID,
         "pymysql",
-        user="root",
-        password="PetsAccessDB4750?",
-        db="pet-shelter-1"
+        user=DB_USER,
+        password=DB_PASS,
+        db=DB_NAME
     )
     return conn
