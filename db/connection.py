ecde98df0099c86ac92b445b9711d73d3da86ae5
michelleegao <163981352+michelleegao@users.noreply.github.com>
2026-04-17 12:55:06 -0400

    fixed connection.py location

---
+++ db/connection.py
@@ -0,0 +1,23 @@
+from google.cloud.sql.connector import Connector
+import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+connector = Connector()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+PROJECT_ID = os.getenv("PROJECT_ID")
+
+def getconn():
+    conn = connector.connect(
+        PROJECT_ID,
+        "pymysql",
+        user=DB_USER,
+        password=DB_PASS,
+        db=DB_NAME
+    )
+    return conn
