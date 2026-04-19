b3ea80384f450c2088613a68fa360597a134d2da
jbw5nv <jbw5nv@virginia.edu>
2026-04-19 11:08:09 -0400

    pets.py and app.py changes

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from flask import Flask, render_template
+## from db import mysql, db_connection, app
+from routes.pets import pet_blueprint
 
 app = Flask(__name__)
+
+app.register_blueprint(pet_blueprint)
 
 # links login HTML page
 @app.route("/")
 def home():
     return render_template('login.html')
-
-# links pets search HTML page
-@app.route("/pets_search")
-def pets():
-    return render_template('/templates/pets_search.html')
-
-# links pets view HTML page
-@app.route("/pets_view")
-def pets_view():
-    return render_template('/templates/pets_view.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
